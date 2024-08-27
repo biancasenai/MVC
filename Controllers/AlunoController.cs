cabae0fd60d8c60ceb3c909e82f4ae5fa4355911
Bianca Furlanetto <bianca.furlanetto440@gmail.com>
2024-08-27 11:54:21 -0300

    iniciando projeto MVc com a listagem de alunos

---
+++ Controllers/AlunoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class AlunoController : Controller
+    {
+        public IActionResult Index()
+        {
+            Aluno a1 = new Aluno(1, "Cristiano de Paula", "cristiano.paula@sp.senai.br", 123456);
+            Aluno a2 = new Aluno(2, "Maria aparecida", "maria.aparecida@sp.senai.br", 2345671);
+            Aluno a3 = new Aluno(3, "José da Silva", "jose.silva@sp.senai.br", 3456712);
+            Aluno a4 = new Aluno(4, "Bianca Furlanetto", "bianca.furlanetto@sp.senai.br", 4567123);
+            Aluno a5 = new Aluno(5, "Helena Cristina", "helena.cristina@sp.senai.br", 5671234);
+
+            //criar uma lista de alunos
+            List<Aluno> listaAlunos = new List<Aluno>();
+            //adicionar os Objetos alunos na lista alunos
+            listaAlunos.Add(a1);
+            listaAlunos.Add(a2);
+            listaAlunos.Add(a3);
+            listaAlunos.Add(a4);
+            listaAlunos.Add(a5);
+            //enviando a lista de alunos por parametro na view index
+            return View(listaAlunos);
+
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Aluno.cs
@@ -0,0 +1,25 @@
+﻿namespace MVC.Models
+{
+    public class Aluno
+    {
+
+        public int AlunoId { get; set; }
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+        public int RM { get; set; }
+
+        //modificar o método construtor
+        //inserir os dados que vieram por parametros nos
+        //atributos do Objeto
+        public Aluno(int id, string nome, string email, int rm)
+        {
+            this.AlunoId = id;
+            this.Nome = nome;
+            this.Email = email;
+            this.RM = rm;
+
+
+        }
+    }
+}
